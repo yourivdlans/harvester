78a7855c263034a6be7e03fcbbbc88c1c94ed208
Youri van der Lans <youri@itflows.nl>
2018-05-03 11:28:58 +0200

    Set up oauth for moneybird.
    So we can retrieve an access token.

---
+++ app/controllers/moneybirds_controller.rb
@@ -0,0 +1,20 @@
+class MoneybirdsController < ApplicationController
+  def show
+    redirect_uri = "http://localhost:3000/moneybird"
+
+    if params['code']
+      access_token_params = {
+        client_id: ENV.fetch('MONEYBIRD_CLIENT_ID'),
+        client_secret: ENV.fetch('MONEYBIRD_CLIENT_SECRET'),
+        code: params['code'],
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      }
+      session[:moneybird_access_token] = JSON.parse(HTTP.post('https://moneybird.com/oauth/token', form: access_token_params).to_s)
+      redirect_to projects_path
+      return
+    end
+
+    redirect_to "https://moneybird.com/oauth/authorize?client_id=#{ENV.fetch('MONEYBIRD_CLIENT_ID')}&redirect_uri=#{redirect_uri}&response_type=code"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :projects, only: :index
+  resource :moneybird, only: :show
 end
