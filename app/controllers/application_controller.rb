29d3c76c0dce8e12a3738df239ccb3d8f7399d3e
Youri van der Lans <youri@itflows.nl>
2018-05-04 08:37:27 +0200

    Delete moneybird session if invalid.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  rescue_from Moneybird::Error, with: :moneybird_error
+
   helper_method :authenticated_with_moneybird?
 
   def authenticated_with_moneybird?
     session[:moneybird_access_token].present?
+  end
+
+  def moneybird_error(exception)
+    if exception.code == 401
+      session.delete(:moneybird_access_token)
+      redirect_to projects_path, alert: 'Your Moneybird token is no longer valid, please re-authenticate.'
+    else
+      redirect_to projects_path, alert: "A moneybird error occurred: #{exception}"
+    end
   end
 end
