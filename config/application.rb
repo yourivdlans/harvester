c798e63785368550a60609ae71ed2526728ac468
Youri van der Lans <youri@itflows.nl>
2019-12-31 12:21:23 +0100

    Revert "Remove active record dependency."
    This reverts commit a06cd428689ae2674440132b6cf45efa7e0dceb6.

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,6 @@
 require_relative 'boot'
 
-require 'rails'
-
-%w(
-  active_storage/engine
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  rails/test_unit/railtie
-  sprockets/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
