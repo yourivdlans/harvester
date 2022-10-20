d76bd0e152bfdf89a63bd73fda27dc36e7f04ff9
Youri van der Lans <youri@itflows.nl>
2022-10-20 12:22:26 +0200

    Sort creditcard accounts in decending order

--- app/controllers/creditcard_transaction_imports_controller.rb
+++ app/controllers/creditcard_transaction_imports_controller.rb
@@ -8,7 +8,7 @@ class CreditcardTransactionImportsController < ApplicationController
     @creditcard_transaction_import = CreditcardTransactionImport.new(creditcard_transaction_import_params)
     @creditcard_transaction_import.creditcard_transactions = RabobankCreditcardPdf.new(@creditcard_transaction_import.pdf.tempfile).parse
 
-    @financial_accounts = Moneybird.new(session[:moneybird_access_token]).financial_accounts
+    @financial_accounts = financial_accounts
   end
 
   private
@@ -21,5 +21,11 @@ class CreditcardTransactionImportsController < ApplicationController
 
   def creditcard_transaction_import_params
     params.require(:creditcard_transaction_import).permit(:pdf)
+  end
+
+  def financial_accounts
+    accounts = Moneybird.new(session[:moneybird_access_token]).financial_accounts
+    accounts = accounts.select { |account| account['type'] == 'CreditCardAccount' }
+    accounts.sort_by { |account| Time.parse(account['created_at']) }.reverse
   end
 end
